9c81187197ac3bb85535ebb161601eaa26b48cc0
giuliojiang <giuliojiang@gmail.com>
2020-04-18 15:52:37 +0100

    simple-server

--- server/src/document.ts
+++ server/src/document.ts
@@ -1,19 +1,46 @@
 import fs from 'fs';
+import path from 'path';
+import cryptoRandomString from 'crypto-random-string';
 
-export class Document {
+export interface Document {
+    name: string;
+    pageFiles: string[];
+    id: string;
+}
 
-    constructor(
-        private readonly name: string,
-        private readonly path: string,
-        private readonly pageFiles: string[]
-    ) { }
-
-    static async fromPath(path: string): Promise<Document> {
-        let files = await fs.promises.readdir(path);
+export class DocumentUtil {
+    static async fromPath(directory: string): Promise<Document> {
+        let files = await fs.promises.readdir(directory);
+        let validFiles: string[] = [];
         for (let f of files) {
-            console.info(f);
+            let fpath = path.resolve(directory, f);
+            let extension = path.extname(fpath).toLowerCase();
+            if (extension == '.png' || extension == '.jpg') {
+                validFiles.push(fpath);
+            }
+        }
+
+        if (validFiles.length > 0) {
+            validFiles.sort();
+            return {
+                name: path.basename(directory),
+                pageFiles: validFiles,
+                id: cryptoRandomString({
+                    length: 16,
+                    type: "hex"
+                })
+            };
+        } else {
+            return null;
+        }
+    }
+
+    static findDocument(documents: Document[], id: string): Document {
+        for (let doc of documents) {
+            if (doc.id == id) {
+                return doc;
+            }
         }
         return null;
     }
-
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,72 @@
 import express from 'express';
+import { Document, DocumentUtil } from './document';
+import fs from 'fs';
+import path from 'path';
 
+let libraryPath = process.argv[2];
+
+interface DocumentModel {
+    name: string;
+    pages: number;
+    id: string;
+}
+
+(async () => {
+    let documents: Document[] = [];
+    let items = await fs.promises.readdir(libraryPath);
+    for (let item of items) {
+        let itemPath = path.resolve(libraryPath, item);
+        let itemStat = await fs.promises.stat(itemPath);
+        if (itemStat.isDirectory()) {
+            let doc = await DocumentUtil.fromPath(itemPath);
+            if (doc != null) {
+                documents.push(doc);
+            }
+        }
+    }
+
+    let app = express();
+
+    // App static files
+    app.use('/app', express.static(path.resolve(__dirname, '..', '..', 'client', 'lib')));
+
+    // Home page redirection
+    app.get('/', (req, res) => {
+        res.redirect('/app/index.html');
+    });
+
+    // Return DocumentModel[]
+    app.get('/api/documents', (req, res) => {
+        let result: DocumentModel[] = [];
+        for (let doc of documents) {
+            result.push({
+                id: doc.id,
+                name: doc.name,
+                pages: doc.pageFiles.length
+            })
+        }
+        res.send(JSON.stringify(result));
+    });
+
+    // Return the page in image format
+    app.get('/api/documents/:id/:page', (req, res) => {
+        let id: string = req.params.id;
+        let page: number = parseInt(req.params.page);
+        let doc = DocumentUtil.findDocument(documents, id);
+        if (doc == null) {
+            res.sendStatus(404);
+            return;
+        }
+        if (page < 0 || page >= doc.pageFiles.length) {
+            res.sendStatus(404);
+            return;
+        }
+
+        res.sendFile(doc.pageFiles[page]);
+        return;
+    });
+
+    app.listen(3001, () => {
+        console.info('Server listening on port 3001');
+    });
+})();
